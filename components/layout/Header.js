8530267a11661fbcde03df2a38541fc514d153e5
Abiezer <abiezer.1530@gmail.com>
2024-11-01 14:29:37 -0300

    className instead of class for navbar

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,11 +6,11 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item"><Link class="nav-link" href="/">Home</Link></li>
-            <li class="nav-item"><Link class="nav-link" href="/about">About</Link></li>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item"><Link class="nav-link" href="/">Home</Link></li>
+            <li className="nav-item"><Link class="nav-link" href="/about">About</Link></li>
           </ul>
         </div>
       </nav>
