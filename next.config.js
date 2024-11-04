41b7f62dc2304506dd1729168850289d73aaf93f
Abiezer <abiezer.1530@gmail.com>
2024-11-04 10:50:57 -0300

    testing load assets 1

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-    output: 'export',
-    basePath: '/me',
-    assetPrefix: '/me/',
-    images: {
-        unoptimized: true
-    }
-}
-  
+  output: 'export',
+  basePath: isProd ? '/me' : '',
+  assetPrefix: isProd ? '/me/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
 module.exports = nextConfig;
